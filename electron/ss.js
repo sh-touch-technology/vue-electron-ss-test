690c0fd8d5050761a25037b957d36fb718a20a42
izumi009 <471624804@qq.com>
2025-02-18 10:38:36 +0800

    添加模块

--- electron/ss.js
+++ electron/ss.js
@@ -1,5 +1,6 @@
 const { load, DataType, open, close, arrayConstructor, define } = require('ffi-rs');
 const os = require('os');
+const fs = require('fs');
 const path = require('path');
 const { printLog } = require('./utils')
 
